a855135f9e5ae0890038726906694eff0a3a5c84
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-17 11:43:24 +0200

    Modification du CSS avant mise des liens entre js et html

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,1 +1,17 @@
-lancerLeJeu();
+// lancerLeJeu();
+
+
+let inputEcriture = document.getElementById("inputEcriture")
+    console.log(inputEcriture);
+
+let btnValiderMot = document.getElementById("btnValiderMot")
+    console.log(btnValiderMot);
+
+let divZoneProposition = document.querySelector("main .zoneProposition")
+    console.log(divZoneProposition);
+
+let spanScore = document.querySelector("main .zoneScore span")
+    console.log(spanScore);
+
+let listeBtnRadio = document.querySelectorAll("main .zoneOptions .optionSource input")
+    console.log(listeBtnRadio);
