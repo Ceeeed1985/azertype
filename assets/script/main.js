159f9d2e659181a4302809676e9a0ff591b9738b
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-17 12:16:07 +0200

    Finalisation avant modification des éléments en js - lien avec html

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,17 +1,17 @@
 // lancerLeJeu();
 
 
-let inputEcriture = document.getElementById("inputEcriture")
-    console.log(inputEcriture);
+// let inputEcriture = document.getElementById("inputEcriture")
+//     console.log(inputEcriture);
 
-let btnValiderMot = document.getElementById("btnValiderMot")
-    console.log(btnValiderMot);
+// let btnValiderMot = document.getElementById("btnValiderMot")
+//     console.log(btnValiderMot);
 
-let divZoneProposition = document.querySelector("main .zoneProposition")
-    console.log(divZoneProposition);
+// let divZoneProposition = document.querySelector("main .zoneProposition")
+//     console.log(divZoneProposition);
 
-let spanScore = document.querySelector("main .zoneScore span")
-    console.log(spanScore);
+// let spanScore = document.querySelector("main .zoneScore span")
+//     console.log(spanScore);
 
-let listeBtnRadio = document.querySelectorAll("main .zoneOptions .optionSource input")
-    console.log(listeBtnRadio);
+// let listeBtnRadio = document.querySelectorAll("main .zoneOptions .optionSource input")
+//     console.log(listeBtnRadio);
