d0a4f8a30982369ac3d60f7538a5c7cd7efe4fad
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-17 09:53:58 +0200

    Séparation en 3 fichiers js

---
+++ assets/script/config.js
@@ -0,0 +1,2 @@
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"];
+const listeMots = ["Cachalot","Pétunia","Serviette"];
--- assets/script/script.js
+++ assets/script/script.js
@@ -1,71 +1,44 @@
+//MES FONCTIONS
 
-
-// JEU
-
-// const listeMots = ["Cachalot","Pétunia","Serviette"];
-// let score = 0;
-
-// let motUtilisateur = prompt("Tapez le mot : "+ listeMots[0]);
-
-// if (motUtilisateur === listeMots[0]){
-//     score ++;
-// } else {
-//     console.log("Vous avez fait une erreur !");
-// }
-
-// motUtilisateur = prompt("Tapez le mot : " + listeMots[1]);
-// if (motUtilisateur === listeMots[1]){
-//     score ++;
-// } else {
-//     console.log("Vous avez fait une erreur");
-// }
-
-// motUtilisateur = prompt("Entrez le mot : " + listeMots[2]);
-
-// if (motUtilisateur === listeMots[2]){
-//     score++;
-// } else {
-//     console.log("Vous avez fait une erreur");
-// }
-
-// alert("Fin du jeu ! Votre score final est de "+ score+ "/3");
-
-const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"];
-const listeMots = ["Cachalot","Pétunia","Serviette"];
-let score = 0;
-let motUtilisateur = null;
-let choix = null;
-let i = null;
-
-listePhrases.push("La vie est une fête !");
-listeMots.push("Gendarme");
-
-
-while ((choix !== "mots") && (choix !== "phrases")){
-    choix = prompt('Voulez-vous une liste de mots ou de phrases ?\n\n- tapez "mots" pour les mots\n- tapez "phrases" pour les phrases');
-}
-
-if (choix === "mots") {
-    for (i=0;i< listeMots.length;i++){
-        motUtilisateur = prompt("Entrez le mot suivant : "+ listeMots[i]);
-        if (motUtilisateur === listeMots[i]){
-            score++;
-        }
-    }
-    alert("Votre score est de : "+score+"/"+ listeMots.length);
-
-} else {
-    for (i=0;i<listePhrases.length;i++){
-        motUtilisateur = prompt("Entrez la phrase suivante : "+ listePhrases[i]);
-        if (motUtilisateur === listePhrases[i]){
-            score++;
-        }
-    }
-    alert("Votre score est de : "+score+"/"+ listePhrases.length);
+function afficherResultat (score, nombreDeMots){
+    alert("Votre score est de : "+score+"/"+ nombreDeMots);
 }
 
 
+function choisirPhrasesOuMots (){
+let choix = prompt('Voulez-vous une liste de mots ou de phrases ?\n\n- tapez "mots" pour les mots\n- tapez "phrases" pour les phrases');
+    while (choix !== "mots" && choix !== "phrases") {
+        choix = prompt('Vous devez choisir entre "mots" ou "phrases"');
+    }
+    return choix;
+}
 
 
+function lancerBoucleDeJeu (listePropositions){
+    let score = 0;
+    for (let i = 0; i < listePropositions.length; i++) {
+        motUtilisateur = prompt("Entrez le mot suivant : "+ listePropositions[i]);
+        if (motUtilisateur === listePropositions[i]){
+            score++;
+        }
+    }
+    return score;
+}
 
+
+function lancerLeJeu () {
+    let choix = choisirPhrasesOuMots ();
+    let score = 0;
+    let nombreDeMots = 0;
+
+        if (choix === "mots") {
+            score = lancerBoucleDeJeu (listeMots);
+            nombreDeMots = listeMots.length;
+        
+        } else {
+            score = lancerBoucleDeJeu (listePhrases);
+            nombreDeMots = listePhrases.length;
+        }
+    afficherResultat (score, nombreDeMots);
+}
 
